c15da44c17a0b6387834bc9cf3f4b6c76a36431d
Hugoren Martinako <aumpfbahn@gmail.com>
2023-01-03 13:11:08 +0100

    Testing auto-commit cron

--- update_data.sh
+++ update_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 query='[out:csv("ref:ine"; true)][timeout:900];area["ISO3166-1"="ES"]->.searchArea;nwr(area.searchArea)["ref:ine"];out;'
-wget -O "ES.OSM.csv" --post-data="${query}" "https://overpass-api.de/api/interpreter"
+wget -O "ES.csv" --post-data="${query}" "https://overpass-api.de/api/interpreter"
 
-node find_missing_ref_ine.js ENTIDADES.UTF8.csv ES.OSM.csv
+node find_missing_ref_ine.js ENTIDADES.csv ES.csv
